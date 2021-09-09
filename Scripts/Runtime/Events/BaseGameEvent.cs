355cfcd995b2784d64571b180574bc675c289f47
Aaron Jackson <yolanother@gmail.com>
2021-09-09 12:44:39 -0700

    Add a generic callback that will match for any event type

--- Scripts/Runtime/Events/BaseGameEvent.cs
+++ Scripts/Runtime/Events/BaseGameEvent.cs
@@ -1,10 +1,85 @@
 ﻿using System;
+using System.Collections.Generic;
+using DoubTech.ScriptableEvents.Listeners.BuiltInTypes;
 using UnityEngine;
 
 namespace DoubTech.ScriptableEvents
 {
     public class BaseGameEvent : ScriptableObject
     {
+        private List<GeneralGameEventListener> listeners = new List<GeneralGameEventListener>();
+        private List<GeneralListenerAction> generalListenerActions = new List<GeneralListenerAction>();
+        private List<Action<object[]>> actionListeners = new List<Action<object[]>>();
+
+        public void AddGeneralListener(GeneralListenerAction action, bool allowDuplicate = false)
+        {
+            if (allowDuplicate || !generalListenerActions.Contains(action))
+            {
+                generalListenerActions.Insert(0, action);
+            }
+        }
+
+        public void RemoveGeneralListener(GeneralListenerAction listener)
+        {
+            generalListenerActions.Remove(listener);
+        }
+
+        public void RegisterGeneralListener(GeneralGameEventListener listener,
+            bool allowDuplicate = false)
+        {
+            if (allowDuplicate || !listeners.Contains(listener))
+            {
+                listeners.Insert(0, listener);
+            }
+        }
+
+        public void UnregisterGeneralListener(GeneralGameEventListener listener)
+        {
+            listeners.Remove(listener);
+        }
+
+        public void RegisterGeneralListener(Action<object[]> listener,
+            bool allowDuplicate = false)
+        {
+            AddGeneralListener(listener, allowDuplicate);
+        }
+
+        public void UnregisterGeneralListener(Action<object[]> listener)
+        {
+            RemoveGeneralListener(listener);
+        }
+
+        public void AddGeneralListener(Action<object[]> listener,
+            bool allowDuplicate = false)
+        {
+            if (allowDuplicate || !actionListeners.Contains(listener))
+            {
+                actionListeners.Insert(0, listener);
+            }
+        }
+
+        public void RemoveGeneralListener(Action<object[]> listener)
+        {
+            actionListeners.Remove(listener);
+        }
+
+        protected virtual void OnInvoked(object[] args)
+        {
+            for (int i = listeners.Count - 1; i >= 0; i--)
+            {
+                if (listeners[i]) listeners[i].OnEventRaised(args);
+            }
+
+            for (int i = generalListenerActions.Count - 1; i >= 0; i--)
+            {
+                generalListenerActions[i].listener.Invoke(generalListenerActions[i], args);
+            }
+
+            for (int i = actionListeners.Count - 1; i >= 0; i--)
+            {
+                actionListeners[i].Invoke(args);
+            }
+        }
         protected virtual void OnInvoke()
         {
             throw new ArgumentException("Not implemented");
@@ -30,29 +105,48 @@ namespace DoubTech.ScriptableEvents
             throw new ArgumentException("Not implemented");
         }
 
+        public void Invoke(object[] args)
+        {
+            if (args.Length == 0) Invoke();
+            if (args.Length == 1) Invoke(args[0]);
+            if (args.Length == 2) Invoke(args[0], args[1]);
+            if (args.Length == 3) Invoke(args[0], args[1], args[2]);
+            if (args.Length == 4) Invoke(args[0], args[1], args[2], args[3]);
+        }
+
         public void Invoke()
         {
+            OnInvoked(new object[0]);
             OnInvoke();
         }
 
         public void Invoke(object a)
         {
+            OnInvoked(new object[] { a });
             OnInvoke(a);
         }
 
         public void Invoke(object a, object b)
         {
+            OnInvoked(new object[] {a, b});
             OnInvoke(a, b);
         }
 
         public void Invoke(object a, object b, object c)
         {
+            OnInvoked(new object[] { a, b, c });
             OnInvoke(a, b, c);
         }
 
         public void Invoke(object a, object b, object c, object d)
         {
+            OnInvoked(new object[] {a, b, c, d});
             OnInvoke(a, b, c, d);
         }
+    }
+
+    public class GeneralListenerAction
+    {
+        public Action<GeneralListenerAction, object[]> listener;
     }
 }
---
+++ Scripts/Runtime/Listeners/GeneralGameEventListener.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using DoubTech.ScriptableEvents.BuiltinTypes;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace DoubTech.ScriptableEvents.Listeners.BuiltInTypes
+{
+    [Serializable]
+    public class GeneralGameEventListener : MonoBehaviour
+    {
+        public BaseGameEvent GameEvent;
+        public GeneralUnityEvent OnEvent;
+
+        protected virtual void OnEnable()
+        {
+            GameEvent.RegisterGeneralListener(this);
+        }
+
+        protected virtual void OnDisable()
+        {
+            GameEvent.UnregisterGeneralListener(this);
+        }
+
+        public virtual void Invoke(object[] args)
+        {
+            GameEvent.Invoke(args);
+        }
+
+        public virtual void OnEventRaised(object[] args)
+        {
+            OnEvent?.Invoke(args);
+        }
+    }
+
+    [Serializable]
+    public class GeneralUnityEvent : UnityEvent<object[]>
+    {
+    }
+}
