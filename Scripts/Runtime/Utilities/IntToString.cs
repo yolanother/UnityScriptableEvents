c9007da3f85499a66dbd98f6bb85bcc729972027
Yolan <yolanother@gmail.com>
2021-12-05 01:17:50 -0800

    Add string formatting to IntToString

--- Scripts/Runtime/Utilities/IntToString.cs
+++ Scripts/Runtime/Utilities/IntToString.cs
@@ -1,26 +1,35 @@
 ﻿using System;
 using DoubTech.ScriptableEvents.Listeners.BuiltInTypes;
+using Unity.VisualScripting.YamlDotNet.Core.Tokens;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Serialization;
 
 namespace DoubTech.ScriptableEvents.Utilities
 {
     public class IntToString : MonoBehaviour
     {
-        [SerializeField]
-        private string format;
+        [FormerlySerializedAs("format")] [SerializeField]
+        private string numberFormat;
+
+        [SerializeField] private string stringFormat;
         [SerializeField] private ValueChangedEvent onValueChanged = new ValueChangedEvent();
 
         public void SetValue(int value)
         {
-            if (!string.IsNullOrEmpty(format))
+            string text;
+            if (!string.IsNullOrEmpty(numberFormat))
             {
-                onValueChanged.Invoke(value.ToString(format));
+                if (null != stringFormat) text = string.Format(stringFormat, value.ToString(numberFormat));
+                else text = value.ToString(numberFormat);
             }
             else
             {
-                onValueChanged.Invoke(value.ToString());
+                if (null != stringFormat) text = string.Format(stringFormat, value);
+                else text = value.ToString();
             }
+
+            onValueChanged.Invoke(text);
         }
 
         [Serializable] public class ValueChangedEvent : UnityEvent<string> {}
