c6a4060bd416ed17c71dea9dec0ddca56ea0b581
Yolan <yolanother@gmail.com>
2022-03-11 21:27:22 -0800

    Add TMP float listener

---
+++ Scripts/Runtime/Utilities/TMPFloatGameEventListener.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using DoubTech.ScriptableEvents.BuiltinTypes;
+using DoubTech.ScriptableEvents.Listeners.BuiltInTypes;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace DoubTech.ScriptableEvents.Utilities
+{
+    [RequireComponent(typeof(Text))]
+    public class TMPFloatGameEventListener : MonoBehaviour
+    {
+        [Header("String Format")]
+        [SerializeField] private string numberFormat;
+        [SerializeField] private string stringFormat;
+
+        [Header("Component Mapping")]
+        [SerializeField] private FloatGameEvent floatGameEvent;
+        [SerializeField] private TextMeshProUGUI textView;
+
+        private void OnValidate()
+        {
+            if (!textView) textView = GetComponent<TextMeshProUGUI>();
+        }
+
+        private void Start()
+        {
+            if (!textView) textView = GetComponent<TextMeshProUGUI>();
+            floatGameEvent.AddListener(SetValue);
+        }
+
+        private void OnDestroy()
+        {
+            floatGameEvent.RemoveListener(SetValue);
+        }
+
+        public void SetValue(float value)
+        {
+            string text;
+            if (!string.IsNullOrEmpty(numberFormat))
+            {
+                if (null != stringFormat) text = string.Format(stringFormat, value.ToString(numberFormat));
+                else text = value.ToString(numberFormat);
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(stringFormat)) text = string.Format(stringFormat, value);
+                else text = value.ToString();
+            }
+
+            textView.text = text;
+        }
+
+        [Serializable] public class ValueChangedEvent : UnityEvent<string> {}
+    }
+}
